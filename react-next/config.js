70931bae11cb4f67d068182d9e07c432ef136838
Guillaume <guillaume.dumoulin@hotmail.fr>
2024-11-27 10:56:47 +0100

    fix: url

--- react-next/config.js
+++ react-next/config.js
@@ -1,9 +1,6 @@
 export function getBaseUrl() {
-  if (process.env.VERCEL_URL) {
-    return `https://${process.env.VERCEL_URL}`;
-  }
-  if (process.env.NEXT_PUBLIC_VERCEL_URL) {
-    return `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
+  if (process.env.VERCEL_PROJECT_PRODUCTION_URL) {
+    return `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`;
   }
   return 'http://localhost:3000';
 }
