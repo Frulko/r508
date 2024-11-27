d2ab71899e3026b8ee14de1d9e5ce294b039c928
Guillaume <guillaume.dumoulin@hotmail.fr>
2024-11-27 16:38:06 +0100

    feat: add supabase for select and insert

--- react-next/components/TodoList.jsx
+++ react-next/components/TodoList.jsx
@@ -1,42 +1,56 @@
-import { config, getBaseUrl } from "@/config";
+import { config, getBaseUrl } from "@/utils/config";
+import { createClient } from "@/utils/supabase/server";
 import { revalidateTag } from "next/cache";
 import React from "react";
 
 async function getData() {
-  console.log('todos: load')
-  const res = await fetch(getBaseUrl() + '/api/todos', {
-    next: { tags: ['todos'] },
-    cache: 'no-store',
-  })
-  return res.json()
+  const supabase = await createClient();
+  const { data: countries } = await supabase.from("todo").select();
+  return countries;
 }
 
 async function TodoList() {
-  const todos = await getData()
+  const todos = await getData();
 
   async function createTodo(formData) {
-    'use server'
-    console.log('todos: create new one')
-    const text = formData.get('text');
-    await fetch(getBaseUrl() + "/api/todos", {
-      method: "POST",
-      body: JSON.stringify({ text }),
-      cache: 'no-store',
-    })
+    "use server";
+    console.log("todos: create new one");
+    const supabase = await createClient();
+    const text = formData.get("text");
+    // await fetch(getBaseUrl() + "/api/todos", {
+    //   method: "POST",
+    //   body: JSON.stringify({ text }),
+    //   cache: 'no-store',
+    // })
+    const { error } = await supabase
+      .from("todo")
+      .insert({ text });
 
-    revalidateTag('todo');
+    console.log('-->', error)
+
+    revalidateTag("todo");
   }
 
   return (
     <div className="container mx-auto flex items-center justify-center h-full">
       <div>
-        <form action={createTodo} className="mb-8 flex flex-col items-start gap-2">
+        <form
+          action={createTodo}
+          className="mb-8 flex flex-col items-start gap-2"
+        >
           <label htmlFor="text" className="flex flex-col">
             <span>Todo: </span>
-            <input type="text" name="text" id="text" className="rounded-md border-black border" />
+            <input
+              type="text"
+              name="text"
+              id="text"
+              className="rounded-md border-black border"
+            />
           </label>
 
-          <button type="submit" className="bg-emerald-400 px-2 py-1 rounded">Ajouter</button>
+          <button type="submit" className="bg-emerald-400 px-2 py-1 rounded">
+            Ajouter
+          </button>
         </form>
         <ul>
           {todos.map((todo, index) => (
